976dc30bf6515fad378b4b96ab00c7fb39ccafea
Nasrin Moshayedi <nasrin@basketasia.com>
2019-12-01 20:56:59 +0330

    why this device part update

--- src/Components/WhyThisDevice/index.js
+++ src/Components/WhyThisDevice/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 import ContentHeader from "../ShareComponent/ContentHeader";
 
 class WhyThisDevice extends React.Component{
     render() {
         return(
-            <ContentHeader title="چرا باید از این دستگاه استفاده گنیم؟"/>
+            <Grid container justify="center">
+                <ContentHeader title="چرا باید از این دستگاه استفاده کنیم؟"/>
+                <Grid item lg={8} style={{marginBottom: 32}}>
+                    <Typography variant="h6">
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                    </Typography>
+                </Grid>
+            </Grid>
         )
     }
 }
