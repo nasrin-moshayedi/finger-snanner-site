a07b8f5d15caf1704ef43148c9d768fe9ea2825a
Nasrin Moshayedi <nasrin@basketasia.com>
2019-12-01 19:35:14 +0330

    main content add

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import Menu from "../Menu";
+import MainContent from "../MainContent";
 
 class Home extends Component{
     render() {
         return (
             <div>
                 <Menu/>
-
+                <MainContent/>
             </div>
         )
     }
---
+++ src/Components/MainContent/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import scannerIcon from "./../../Images/scanner.png";
+import Typography from "@material-ui/core/Typography";
+
+class MainContent extends React.Component{
+    render() {
+        return(
+            <Grid container justify="center" alignItems="center" style={{margin: "50px auto"}}>
+                <Grid item lg={4} style={{textAlign: "center"}}>
+                    <img src={scannerIcon} width={300} />
+                </Grid>
+                <Grid item lg={7} style={{padding: 32}}>
+                    <Typography variant="h5" style={{paddingBottom: 32}}>
+                        دستگاه اثر انگشت آرین (T-338321)
+                    </Typography>
+                    <Typography variant="subtitle2">
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                        این دستگاه از جدیدیترین و پیشرفته ترین تکنولوژی ها برای شناسایی اثر انگشت برا خوردار است.
+                    </Typography>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default MainContent;
